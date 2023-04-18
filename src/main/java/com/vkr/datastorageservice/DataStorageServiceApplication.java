04b8025350ec4ef81d7fe84fc4c275d6f93ea989
aluk0220 <aleksandr.ukhov@netcracker.com>
2023-04-19 00:50:56 +0400

    Initial commit

---
+++ src/main/java/com/vkr/datastorageservice/DataStorageServiceApplication.java
@@ -0,0 +1,14 @@
+package com.vkr.datastorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication()
+public class DataStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStorageServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/vkr/datastorageservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.vkr.datastorageservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DataStorageServiceApplication.class);
+	}
+
+}
